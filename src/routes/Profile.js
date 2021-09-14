13a017b1622b228d5f2d74dd7d936ce7f86d0b25
Webdornald <webdornald@gmail.com>
2021-09-15 01:40:06 +0900

    nwitter done

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -27,18 +27,29 @@ const Profile = ({userObj, refreshUser}) => {
     }
 
     return (
-        <>
-            <form action="" onSubmit={onSubmit}>
+        <div className={"container"}>
+            <form action="" onSubmit={onSubmit} className={"profileForm"}>
                 <input
                     onChange={onChange}
                     type="text"
                     placeholder={"Dispaly name"}
                     value={newDisplayName}
+                    autoFocus={true}
+                    className={"formInput"}
                 />
-                <input type="submit" value={"Update Profile"}/>
+                <input
+                    type="submit"
+                    value={"Update Profile"}
+                    className={"formBtn"}
+                    style={{
+                        marginTop: 10,
+                    }}
+                />
             </form>
-            <button onClick={onLogOutClick}>Log Out</button>
-        </>
+            <span className={"formBtn cancelBtn logOut"} onClick={onLogOutClick}>
+                Log Out
+            </span>
+        </div>
     )
 }
 
